58d71ea9dac437f1d68261f8ec95db51b5f2d329
Sara Linsley <sara.linsley@gmail.com>
2024-01-29 19:16:53 -0500

    add dev mode

--- Letterer-Helper/webpack.config.js
+++ Letterer-Helper/webpack.config.js
@@ -1,7 +1,27 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/inputs.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'inputs.bundle.js'
+  },
+  resolve: {
+    alias: {
+      'node_modules': path.join(__dirname, 'node_modules'),
+    },
+    fallback: {
+      'indesign': false,
+      'uxp': false
+    },
+  },
+  target: 'node'
+},
+{
+  mode: 'development',
+  entry: './src/inputs.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'inputs.bundle.js'
